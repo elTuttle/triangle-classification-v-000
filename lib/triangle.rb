3598cb99d0ecca96201b281caec818d6723e3e52
John Tuttle <thejwt@gmail.com>
2017-10-02 18:13:47 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,9 +9,9 @@ class Triangle
       if side_one == side_two && side_one == side_three
         return :equilateral
       elsif side_one != side_two && side_one != side_three && side_two =! side_three
-        return :isosceles
-      elsif ((side_one == side_two && side_one != side_three) || (side_one == side_three && side_one != side_two) || (side_two == side_three && side_two != side_one))
         return :scalene
+      elsif ((side_one == side_two && side_one != side_three) || (side_one == side_three && side_one != side_two) || (side_two == side_three && side_two != side_one))
+        return :isosceles
       end
     end
   end
