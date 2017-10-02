6577562c197a279d964c66ac6b7d769505a43949
John Tuttle <thejwt@gmail.com>
2017-10-02 18:45:01 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,12 +19,16 @@ class Triangle
       largest_side = @side_two
       small_side1 = @side_one
       small_side2 = @side_three
+    elsif @side_one > @side_two && @side_one > @side_three
+      largest_side = @side_one
+      small_side1 = @side_two
+      small_side2 = @side_three
     end
     if (@side_one > 0 && @side_two > 0 && @side_three > 0)
       if @side_one == @side_two && @side_one == @side_three
         return :equilateral
       elsif @side_one != @side_two && @side_one != @side_three && @side_two != @side_three
-        
+
         return :scalene
       elsif ((@side_one == @side_two && @side_one != @side_three) || (@side_one == @side_three && @side_one != @side_two) || (@side_two == @side_three && @side_two != @side_one))
         return :isosceles
