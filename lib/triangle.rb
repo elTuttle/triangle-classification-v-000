b39733bb9a4263e10988a2224d2b74205db43f46
John Tuttle <thejwt@gmail.com>
2017-10-02 18:05:08 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,4 +1,13 @@
 class Triangle
+  attr_reader :side_one, :side_two, :side_three, :equilateral, :isosceles, :scalene
+  
   def initialize(side_one,side_two,side_three)
-  end  
+  end
+
+  def kind
+    if side_one == side_two && side_one == side_three && side_one > 0
+      
+    end
+  end
+  
 end
