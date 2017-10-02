e6ccf5234547e244661660d415fc025e56ccb0f5
John Tuttle <thejwt@gmail.com>
2017-10-02 18:44:46 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,10 +8,23 @@ class Triangle
   end
 
   def kind
-    if (@side_one > 0 && @side_two > 0 && @side_three > 0) && ((@side_one + @side_two) > @side_three || (@side_one + @side_three) > @side_two || (@side_two + @side_three) > @side_one)
+    largest_side = 0
+    small_side1 = 0
+    small_side2 = 0
+    if @side_one > @side_two && @side_one > @side_three
+      largest_side = @side_one
+      small_side1 = @side_two
+      small_side2 = @side_three
+    elsif @side_two > @side_one && @side_two > @side_three
+      largest_side = @side_two
+      small_side1 = @side_one
+      small_side2 = @side_three
+    end
+    if (@side_one > 0 && @side_two > 0 && @side_three > 0)
       if @side_one == @side_two && @side_one == @side_three
         return :equilateral
       elsif @side_one != @side_two && @side_one != @side_three && @side_two != @side_three
+        
         return :scalene
       elsif ((@side_one == @side_two && @side_one != @side_three) || (@side_one == @side_three && @side_one != @side_two) || (@side_two == @side_three && @side_two != @side_one))
         return :isosceles
